09ed71af2788d089319323d8467ed65e4bb7f385
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-06-13 19:21:10 +0300

    Created Inheritance with Shapre Classes

--- task-10/main.py
+++ task-10/main.py
@@ -9,8 +9,32 @@
 # - The `Circle` class should be initialized with radius.
 # - Both derived classes should implement the `area` method to calculate and return the area of the shape.
 
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius ** 2
+    
 # Examples:
-# rectangle = Rectangle(4, 5)
-# circle = Circle(3)
-# print(rectangle.area())  # Expected: 20
-# print(circle.area())     # Expected: 28.274333882308138
+rectangle = Rectangle(4, 5)
+circle = Circle(3)
+print(rectangle.area())  # Expected: 20
+print(circle.area())     # Expected: 28.274333882308138
