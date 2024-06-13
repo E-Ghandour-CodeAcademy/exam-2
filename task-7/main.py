6a2ee5a41781f1ec90c860041d777c2f1e3fba0f
Eslam Ghandour <eslam.aly.handour@codeacademylt.onmicrosoft.com>
2024-06-13 19:11:30 +0300

    Created Book class

--- task-7/main.py
+++ task-7/main.py
@@ -5,7 +5,15 @@
 # - `get_author()`: Returns "Author: " followed by the instance author.
 
 class Book:
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def get_title(self):
+        return f"Title: {self.title}"
+    
+    def get_author(self):
+        return f"Author: {self.author}"
 
 PP = Book("Pride and Prejudice", "Jane Austen")
 H = Book("Hamlet", "William Shakespeare")
